96c7f65049a61733b7e8e4e612e27d2795310927
Denis Osyushkin <denisitch@gmail.com>
2023-12-03 20:35:11 +0300

    add homework-1

--- src/main/java/com/denisitch/controllers/FirstController.java
+++ src/main/java/com/denisitch/controllers/FirstController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Objects;
+
 @Controller
 @RequestMapping("/first")
 public class FirstController {
@@ -20,5 +22,26 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodbyePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a", required = false) int a,
+                             @RequestParam(value = "b", required = false) int b,
+                             @RequestParam(value = "action", required = false) String action,
+                             Model model) {
+        if (Objects.equals(action, "addition")) {
+            model.addAttribute("message",
+                    "Result " + a + " " + action + " " + b + " equals " + (a + b));
+        } else if (Objects.equals(action, "subtraction")) {
+            model.addAttribute("message",
+                    "Result " + a + " " + action + " " + b + " equals " + (a - b));
+        } else if (Objects.equals(action, "multiplication")) {
+            model.addAttribute("message",
+                    "Result " + a + " " + action + " " + b + " equals " + (a * b));
+        } else if (Objects.equals(action, "division")) {
+            model.addAttribute("message",
+                    "Result " + a + " " + action + " " + b + " equals " + (a / (double) b));
+        }
+        return "first/calculator";
     }
 }
