72c65a6367d6a1cc6a171ac9ef394f115d7fa814
Denisitch <denisitch@gmail.com>
2023-12-04 15:28:54 +0300

    add READ from CRUD

---
+++ src/main/java/com/denisitch/dao/PersonDAO.java
@@ -0,0 +1,29 @@
+package com.denisitch.dao;
+
+import com.denisitch.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> id == person.getId()).findAny().orElse(null);
+    }
+}
